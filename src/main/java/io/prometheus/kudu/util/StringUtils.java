cbaca438a581921baa58087c69a4a3d0fdc76196
ZhangLei <RyanCheung98@163.com>
2021-07-19 18:54:17 +0800

    function 'getOrDefault(str0, str1)' to get the string value that if the 'str0' is empty, it will return 'str1'.

--- src/main/java/io/prometheus/kudu/util/StringUtils.java
+++ src/main/java/io/prometheus/kudu/util/StringUtils.java
@@ -9,4 +9,13 @@ public class StringUtils {
     public static boolean isEmpty(String str) {
         return str.isEmpty();
     }
+
+    public static String getOrDefault(String str, String defaultStr) {
+        return getOrDefault(str, defaultStr);
+    }
+
+    public static String getOrDefault(Object str, Object defaultStr) {
+        return isEmpty(str) ? defaultStr.toString() : str.toString();
+    }
+
 }
