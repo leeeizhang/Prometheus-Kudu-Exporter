98f1bc8505ed80bc30fd5b9f837737e4fb86d09e
ZhangLei <RyanCheung98@163.com>
2021-07-23 15:34:34 +0800

    PushGatewayReporter improve

--- src/main/java/io/prometheus/kudu/reporter/KuduMetricPushGatewayReporter.java
+++ src/main/java/io/prometheus/kudu/reporter/KuduMetricPushGatewayReporter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright RyanCheung98@163.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.prometheus.kudu.reporter;
 
 import io.prometheus.client.Collector;
@@ -8,13 +24,24 @@ import io.prometheus.kudu.task.KuduExporterTask;
 import io.prometheus.kudu.util.LoggerUtils;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Push-Gateway Reporter
+ */
 public class KuduMetricPushGatewayReporter extends KuduExporterTask<List<Map<?, ?>>> {
     private Collector kuduMetricCollector;
     private PushGateway pushGateway;
 
+    /**
+     * Constructor to init the Reporter
+     *
+     * @param threadID      the id of current reporter thread
+     * @param configuration exporter configuration
+     * @param metricPool    metric pool to get metric resources
+     */
     public KuduMetricPushGatewayReporter(
             Integer threadID,
             KuduExporterConfiguration configuration,
@@ -22,19 +49,37 @@ public class KuduMetricPushGatewayReporter extends KuduExporterTask<List<Map<?, 
         super(threadID, configuration, metricPool);
     }
 
+    /**
+     * Generate the PushGateway and Collector
+     */
     @Override
-    protected void start() throws Exception {
-        this.kuduMetricCollector = new KuduMetricGeneralCollector(configuration, metricPool);
-        this.pushGateway = new PushGateway(configuration.getPushGatewayReporterHost());
-        this.pushGateway.push(kuduMetricCollector, "kudu");
+    protected void start() {
+        try {
+            this.kuduMetricCollector = new KuduMetricGeneralCollector(configuration, metricPool);
+            this.pushGateway = new PushGateway(configuration.getPushGatewayReporterHost());
+            this.pushGateway.push(kuduMetricCollector, "kudu");
+        } catch (IOException e) {
+            logger.warn("push-gateway reporter start error.", e);
+        }
     }
 
+    /**
+     * Report metrics into push-gateway periodically
+     */
     @Override
-    protected void process() throws Exception {
-        this.pushGateway.push(kuduMetricCollector, "kudu");
+    protected void process() {
+        try {
+            this.pushGateway.push(kuduMetricCollector, "kudu");
+        } catch (IOException e) {
+            logger.warn("fail to report metrics to push-gateway.", e);
+        }
     }
 
+    /**
+     * Stop server when thread destroy
+     */
     @Override
     protected void stop() {
     }
+
 }
